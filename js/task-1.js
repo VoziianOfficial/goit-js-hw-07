4d499a1ad0228382508d7598c9342036342bfcc8
Yuliia <vozianulia@gmail.com>
2024-06-24 22:33:12 +0300

    add and upate file task 1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+
+const categories = document.getElementById("categories");
+
+const items = categories.querySelectorAll(".item");
+
+console.log(`Amount of categories: ${items.length}`);
+
+items.forEach(item => {
+    const title = item.querySelector("h2").textContent;
+
+    const elementAmount = item.querySelectorAll("ul li").length;
+
+    console.log(`Category: ${title}`);
+    console.log(`Element: ${elementAmount}`);
+});
