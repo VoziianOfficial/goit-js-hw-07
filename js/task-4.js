5cab224ecb19e0dcc619c733a7166a8de311e54b
Yuliia <vozianulia@gmail.com>
2024-06-26 23:13:18 +0300

    add tasks 4,5,6

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", event => {
+    event.preventDefault(); 
+
+    const form = event.target;
+    const emailValue = form.elements.email.value.trim();
+    const passwordValue = form.elements.password.value.trim();
+
+    if (emailValue === "" || passwordValue === "") {
+        return alert("'All form fields must be filled in'");
+    } else {
+        const formResult = {
+            email: emailValue,
+            password: passwordValue
+        };
+
+        console.log(formResult);
+
+        event.target.reset();
+    }
+})
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonColor = document.querySelector(".change-color");
+const spanColor= document.querySelector(".color");
+
+  buttonColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+
+});
